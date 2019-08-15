1df4bc2f5d83d7e2f9ca696290d74736e451a332
xiongtj <xiongtj@gmail.com>
2019-08-15 14:42:56 +0800

    默认显示标题栏，延时300毫秒关闭

--- cmlweex/src/main/java/com/didi/chameleon/weex/container/CmlWeexActivity.java
+++ cmlweex/src/main/java/com/didi/chameleon/weex/container/CmlWeexActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.opengl.Visibility;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -179,7 +180,14 @@ public class CmlWeexActivity extends CmlContainerActivity implements CmlWeexInst
         objectView = view;
         viewContainer.addView(view);
         loadingView.setVisibility(View.GONE);
-        titleView.setVisibility(View.GONE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                titleView.setVisibility(View.GONE);
+            }
+        },300);
+
+
     }
 
     @Override
