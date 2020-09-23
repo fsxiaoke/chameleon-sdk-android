ee3ac35ef428430756ab512530d4f0a9e9dc4fab
zhaodsh <zhaodsh@fxiaoke.com>
2020-09-23 14:34:05 +0800

    add

--- cmlweex/src/main/java/com/didi/chameleon/weex/container/CmlWeexActivity.java
+++ cmlweex/src/main/java/com/didi/chameleon/weex/container/CmlWeexActivity.java
@@ -231,14 +231,14 @@ public class CmlWeexActivity extends CmlContainerActivity implements CmlWeexInst
                 }
 
                 loadingView.setVisibility(View.GONE);
-                refreshView.setVisibility(View.VISIBLE);
+                reload.setVisibility(View.VISIBLE);
 //
 //                if(isDevPackage()){
 //                    if(degradeCode == 4){
 //                        showDialog("Bundle下载失败，请检查环境和配置");
 //                    }
 //                }else{
-//                    refreshView.setVisibility(View.VISIBLE);
+//                    reload.setVisibility(View.VISIBLE);
 //                }
             }
         });
